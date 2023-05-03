51b180970b70a029354f3cde6980632959f43327
daimessdn@gmail.com <daimessdn@gmail.com>
2023-05-04 04:59:44 +0700

    working with POST method

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
+
+class User(BaseModel):
+    name: str
+    age: int
+    email: Optional[str] = None
 
 # init FastAPI instance
 app = FastAPI()
@@ -11,11 +17,9 @@ app = FastAPI()
 def index():
     return {"response": "hello, world!"}
 
-
 @app.get("/about")
 def about():
     return {"response": "about"}
-
 
 # Working with parameters
 users = {
@@ -24,7 +28,6 @@ users = {
     3: {"name": "Surtiyem", "age": 13},
 }
 
-
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     return users[user_id]
@@ -32,9 +35,8 @@ def get_user(user_id: int):
 # Using Path method in FastAPI
 # Import Path first from fastapi module
 
-
 @app.get("/users-2/{user_id}")
-def get_user_2(user_id: int = Path(description="ID of user for wiewing user details.", gt=0, lt=4)):
+def get_user_2(user_id: int = Path(description="ID of user for wiewing user details.", gt=0, lt=len(users))):
     return users[user_id]
 
 # Working in queries
@@ -45,3 +47,16 @@ def get_user_3(*, name: Optional[str] = None, test: int = None):
             return { "user": users[user_id], "test": test }
         
     return { "response": "404 not found" }
+
+# working with POST method
+@app.post("/users/{user_id}")
+def create_user(*, user_id: int = len(users) + 1, user: User):
+    if user_id in users:
+        return { "error": "user already exists" }
+    
+    users[user_id] = {
+        "name": user.name,
+        "age": user.age,
+        "email": user.email
+    }
+    return users[user_id]
