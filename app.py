b409ff62692fc67ed762a913229b7630899fd2f8
daimessdn@gmail.com <daimessdn@gmail.com>
2023-05-04 04:23:22 +0700

    working with parameters

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
+# init FastAPI instance
 app = FastAPI()
 
-
+# Adding route of GET endpoints
 @app.get("/")
 def index():
     return {"response": "hello, world!"}
@@ -10,4 +11,14 @@ def index():
 @app.get("/about")
 def about():
     return {"response": "about"}
-    
+
+# Working with parameters
+users = {
+    1: { "name": "Bambang", "age": 20 },
+    2: { "name": "Sarah", "age": 18 },
+    3: { "name": "Surtiyem", "age": 13 },
+}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return users[user_id]
