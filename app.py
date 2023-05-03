d9203af3785e85dd31d8ca10213814eed7231e94
daimessdn@gmail.com <daimessdn@gmail.com>
2023-05-04 04:36:59 +0700

    added less than and greater than parameters

--- app.py
+++ app.py
@@ -4,28 +4,34 @@ from fastapi import FastAPI, Path
 app = FastAPI()
 
 # Adding route of GET endpoints
+
+
 @app.get("/")
 def index():
     return {"response": "hello, world!"}
+
 
 @app.get("/about")
 def about():
     return {"response": "about"}
 
+
 # Working with parameters
 users = {
-    1: { "name": "Bambang", "age": 20 },
-    2: { "name": "Sarah", "age": 18 },
-    3: { "name": "Surtiyem", "age": 13 },
+    1: {"name": "Bambang", "age": 20},
+    2: {"name": "Sarah", "age": 18},
+    3: {"name": "Surtiyem", "age": 13},
 }
+
 
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     return users[user_id]
 
 # Using Path method in FastAPI
-## Import Path first from fastapi module
+# Import Path first from fastapi module
+
 
 @app.get("/users-2/{user_id}")
-def get_user_2(user_id: int = Path(description="ID of user for wiewing user details.")):
+def get_user_2(user_id: int = Path(description="ID of user for wiewing user details.", gt=0, lt=4)):
     return users[user_id]
